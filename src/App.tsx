b4be601c66edfb9133e8be270fb058d1a7b0e455
Brian Adams <brianadamsmaino68@gmail.com>
2023-09-25 09:19:59 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import Task from './Task';
+
+interface TaskData {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<TaskData[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const addTask = () => {
+    if (newTaskTitle.trim() === '') return;
+
+    const newTask: TaskData = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
+  };
+
+  const deleteTask = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTask = (id: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        placeholder="New task..."
+        value={newTaskTitle}
+        onChange={(e) => setNewTaskTitle(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+
+      <div className="task-list">
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            title={task.title}
+            completed={task.completed}
+            onDelete={deleteTask}
+            onToggle={toggleTask}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Task.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+interface TaskProps {
+  id: number;
+  title: string;
+  completed: boolean;
+  onDelete: (id: number) => void;
+  onToggle: (id: number) => void;
+}
+
+const Task: React.FC<TaskProps> = ({
+  id,
+  title,
+  completed,
+  onDelete,
+  onToggle,
+}) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(title);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+    // You can add code here to save the newTitle in your task list
+  };
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => onToggle(id)}
+      />
+      {isEditing ? (
+        <input
+          type="text"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          onBlur={handleSave}
+          autoFocus
+        />
+      ) : (
+        <span onClick={handleEdit}>{title}</span>
+      )}
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </div>
+  );
+};
+
+export default Task;
